4edef743f5fa55706cb93156548ef781b4140f88
Sudheer <sudheer.gadhala@kagamierp.com>
2022-02-22 22:09:04 +0530

    aggrid

---
+++ packages/client/src/assets/scripts/ag-grid/ag-grid-community/lib/interfaces/iChartOptions.d.ts
@@ -0,0 +1,7 @@
+export declare type ChartType = 'column' | 'groupedColumn' | 'stackedColumn' | 'normalizedColumn' | 'bar' | 'groupedBar' | 'stackedBar' | 'normalizedBar' | 'line' | 'scatter' | 'bubble' | 'pie' | 'doughnut' | 'area' | 'stackedArea' | 'normalizedArea' | 'histogram' | 'columnLineCombo' | 'areaColumnCombo' | 'customCombo';
+export declare type ChartMenuOptions = 'chartSettings' | 'chartData' | 'chartFormat' | 'chartLink' | 'chartUnlink' | 'chartDownload';
+export interface SeriesChartType {
+    colId: string;
+    chartType: ChartType;
+    secondaryAxis?: boolean;
+}
---
+++ packages/client/src/assets/scripts/ag-grid/ag-grid-react/lib/shared/keyGenerator.d.ts
@@ -0,0 +1,2 @@
+// ag-grid-react v27.0.1
+export default function generateNewKey(): string;
